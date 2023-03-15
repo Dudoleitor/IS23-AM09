4f14cf24b8ebf8105f21cc2f1d99032aaa58026e
Mik-Ado <125997032+Mik-Ado@users.noreply.github.com>
2023-03-15 17:29:52 +0100

    Completed Shelf methods

--- src/main/java/it/polimi/ingsw/shared/Shelf.java
+++ src/main/java/it/polimi/ingsw/shared/Shelf.java
@@ -1,15 +1,51 @@
 package it.polimi.ingsw.shared;
 
 public class Shelf {
-    private Tile tiles[][];
-    /*public Shelf() { //incomplete function
-
-    }*/
+    private final Tile tiles[][];
+    public Shelf(int rows, int columns) {
+        tiles = new Tile[rows][columns];
+        for (int i = 0; i<rows; i++){
+            for (int j = 0; j<columns; j++){
+                tiles[i][j] = Tile.Empty;
+            }
+        }
+    }
     public Tile getTile(Position pos){
         return tiles[pos.getRow()][pos.getColumn()];
     }
 
-    /*public int getHighestColumn(){ //incomplete function
-    }*/
+    public int getHighestColumn(){
+        int max = 0;
+        boolean max_found = false;
+        int count;
+        boolean not_empty;
+
+        for(int j = 0; !max_found && j < tiles[0].length; j++){
+            count = 0;
+            not_empty = false;
+            for(int i = 0; !max_found && !not_empty && i < tiles.length; i++){
+                if(tiles[i][j] == Tile.Empty){
+                    count++;
+                } else {
+                    not_empty = true;
+                }
+                if(count == tiles.length)
+                    max_found = true;
+            }
+            if(count > max)
+                max = count;
+        }
+        return max;
+    }
+    public void insertTile(Tile tile, int column){
+        boolean is_empty = false;
+        for(int i = tiles.length-1; !is_empty && i>=0; i--){
+            if(tiles[i][column] == Tile.Empty){
+                tiles[i][column] = tile;
+                is_empty = true;
+            }
+        }
+
+    }
 
 }
