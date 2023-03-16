2e6a3c44b7a44d22d159afc5e2d7e19768e1464f
Edoardo Carlotto <edoardo.carlotto@mail.polimi.it>
2023-03-16 08:38:21 +0100

    Updated tests and javadoc related to PlayerGoal

--- src/main/java/it/polimi/ingsw/server/PlayerGoal.java
+++ src/main/java/it/polimi/ingsw/server/PlayerGoal.java
@@ -17,11 +17,22 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+/**
+ * This class loads a player's goal from a json file and
+ * implements the method to calculate the amount of
+ * points achieved by the player.
+ */
 public class PlayerGoal {
     private final List<GoalPosition> positionList;
     private final JSONObject pointsMap;
     private final int goalId;
 
+    /**
+     * This constructor is used when the goalId is known
+     * @param jsonPath Path to the json file
+     * @param goalId id of the goal to load
+     * @throws PlayerGoalLoadingException when a parsing error happens
+     */
     public PlayerGoal(String jsonPath, int goalId) throws PlayerGoalLoadingException {
         this.goalId = goalId;
         JSONParser jsonParser = new JSONParser();
@@ -51,6 +62,11 @@ public class PlayerGoal {
         }
     }
 
+    /**
+     * This constructor is used when the goalId needs to be chosen randomly
+     * @param jsonPath Path to the json file
+     * @throws PlayerGoalLoadingException when a parsing error happens
+     */
     public PlayerGoal(String jsonPath) throws PlayerGoalLoadingException {
         JSONParser jsonParser = new JSONParser();
         Random rand = new Random();
@@ -83,6 +99,12 @@ public class PlayerGoal {
 
     }
 
+    /**
+     * This function parses a single json object and returns a GoalPosition
+     * @param jsonBlock json object to be parsed
+     * @return GoalPosition, parsing result
+     * @throws PlayerGoalLoadingException when a parsing error happens
+     */
     private GoalPosition parsePosition(JSONObject jsonBlock) throws PlayerGoalLoadingException {
         try {
             String tileStr = jsonBlock.get("tileName").toString();
@@ -96,14 +118,27 @@ public class PlayerGoal {
         }
     }
 
-    private boolean checkSinglePosition(Shelf shelf, GoalPosition pos){
+    /**
+     * This predicate checks if a single tile is in the correct position
+     * @param shelf Player's shelf
+     * @param pos GoalPosition to be checked
+     * @return true/false
+     */
+    private boolean checkSinglePosition(Shelf shelf, GoalPosition pos) {
         return pos.getTile()
                 .equals(
                         shelf.getTile(pos.getPos())
                 );
     }
 
+    /**
+     * This function maps the number of correct positions to the point achieved
+     * @param c Correct position counted
+     * @return Points achieved
+     * @throws PlayerGoalLoadingException when a parsing error happens
+     */
     private int countToPoints(int c) throws PlayerGoalLoadingException {
+        if (c==0) return 0;
         try {
             return Math.toIntExact(
                     (Long) pointsMap.get(String.valueOf(c))
@@ -112,6 +147,14 @@ public class PlayerGoal {
             throw new PlayerGoalLoadingException("Error while parsing json: wrong point map");
         }
     }
+
+    /**
+     * This function uses the player's shelf and the properties of the
+     * goal to calculate the amount of points achieved.
+     * @param shelf player's shelf
+     * @return points achieved
+     * @throws PlayerGoalLoadingException when a parsing error happens
+     */
     public int check(Shelf shelf) throws PlayerGoalLoadingException {
         int count = positionList
                 .stream()
@@ -153,7 +196,7 @@ class GoalPosition {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GoalPosition that = (GoalPosition) o;
-        return Objects.equals(pos, that.pos) && tile == that.tile;
+        return Objects.equals(pos, that.getPos()) && tile == that.getTile();
     }
 
     @Override
--- src/test/java/it/polimi/ingsw/server/PlayerGoalTest.java
+++ src/test/java/it/polimi/ingsw/server/PlayerGoalTest.java
@@ -1,5 +1,4 @@
 package it.polimi.ingsw.server;
-import it.polimi.ingsw.shared.Position;
 import it.polimi.ingsw.shared.Tile;
 import it.polimi.ingsw.shared.Shelf;
 
@@ -46,5 +45,57 @@ public class PlayerGoalTest {
          assertEquals("Error while parsing json: wrong position attributes", e.getMessage());
      }
 
-     //TODO Add more tests
+    @Test
+    void WrongPos() {
+        String jsonPath = "src/test/resources/WrongPos.json";
+        Exception e = assertThrows(PlayerGoalLoadingException.class, () -> new PlayerGoal(jsonPath));
+        assertEquals("Error while parsing json: wrong position attributes", e.getMessage());
+    }
+
+    @Test
+    void MultipleGoals() {  // Testing if the goals is randomly chosen correctly
+        String jsonPath = "src/test/resources/MultipleGoals.json";
+
+        PlayerGoal goal = new PlayerGoal(jsonPath);
+        assertTrue(goal.getGoalId() >= 0);
+        assertTrue(goal.getGoalId() < 4);
+
+        goal = new PlayerGoal(jsonPath);
+        assertTrue(goal.getGoalId() >= 0);
+        assertTrue(goal.getGoalId() < 4);
+
+        goal = new PlayerGoal(jsonPath);
+        assertTrue(goal.getGoalId() >= 0);
+        assertTrue(goal.getGoalId() < 4);
+    }
+    @Test
+    void TestGoal() {  // Inserting tiles one at a time and checking behaviour
+        String jsonPath = "src/test/resources/TestGoal.json";
+        Shelf shelf = new Shelf(3, 3);
+        PlayerGoal goal = new PlayerGoal(jsonPath);
+
+        assertEquals(0, goal.getGoalId());
+
+        assertEquals(0, goal.check(shelf));  // Empty shelf
+
+        shelf.insertTile(Tile.Cat, 0);
+        assertEquals(1, goal.check(shelf));  // One matches
+
+        shelf.insertTile(Tile.Book, 0);
+        assertEquals(3, goal.check(shelf));  // Two matches
+
+        shelf.insertTile(Tile.Trophy, 0);
+        assertEquals(5, goal.check(shelf));  // Three matches
+
+        shelf.insertTile(Tile.Plant, 2);
+        assertEquals(5, goal.check(shelf));  // Nothing changes
+        shelf.insertTile(Tile.Frame, 2);
+        assertEquals(5, goal.check(shelf));  // Nothing changes
+
+        shelf.insertTile(Tile.Game, 2);
+        assertEquals(8, goal.check(shelf));  // Four matches
+
+        shelf.insertTile(Tile.Plant, 1);  // Wrong tile
+        assertEquals(8, goal.check(shelf));  // Nothing changes
+    }
 }
