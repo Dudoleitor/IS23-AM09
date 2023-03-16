2052c635db5a0d1a6282868b3b1635df66a5d65c
Edoardo Carlotto <edoardo.carlotto@mail.polimi.it>
2023-03-16 08:37:07 +0100

    Minor fixes

--- src/main/java/it/polimi/ingsw/shared/Tile.java
+++ src/main/java/it/polimi/ingsw/shared/Tile.java
@@ -1,23 +1,12 @@
 package it.polimi.ingsw.shared;
 
 public enum Tile {
-    Cat("cat"),
-    Book("book"),
-    Game("game"),
-    Frame("frame"),
-    Trophie("trophie"),
-    Plant("plant"),
+    Cat,
+    Book,
+    Game,
+    Frame,
+    Trophy,
+    Plant,
     Empty,
-    Invalid;
-
-    private final String name;
-    private String getName() { return this.name; }
-
-    Tile() {
-        this.name = new String("not_applicable");
-    }
-    Tile(String name) {
-        this.name = new String(name);
-    }
-
+    Invalid
 }
