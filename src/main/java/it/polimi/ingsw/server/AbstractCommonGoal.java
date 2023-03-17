5f40d127ab29309c2f0e0276ce70cf5833b20a5d
Mik-Ado <125997032+Mik-Ado@users.noreply.github.com>
2023-03-17 16:29:26 +0100

    Added CommonGoalsException Modified AbstractCommonGoal methods to throw exceptions

--- src/main/java/it/polimi/ingsw/server/AbstractCommonGoal.java
+++ src/main/java/it/polimi/ingsw/server/AbstractCommonGoal.java
@@ -11,7 +11,7 @@ public abstract class AbstractCommonGoal {
 
     public abstract int getID();
     protected abstract boolean check(Shelf shelf);
-    public void populatePointsStack(int number_of_players){
+    public void populatePointsStack(int number_of_players) throws CommonGoalsException{
         this.points = new Stack<>();
         switch (number_of_players){
             case 2:
@@ -30,7 +30,7 @@ public abstract class AbstractCommonGoal {
                 points.push(8);
                 break;
             default:
-                //return exception or do nothing?
+                throw new CommonGoalsException("Error while Populating PointsStack : Illegal num of players");
         }
     }
 
@@ -41,29 +41,51 @@ public abstract class AbstractCommonGoal {
     public ArrayList<Integer> showPointsStack(){ //useful in debugging
         return new ArrayList<>(points);
     }
-    protected boolean notEmptyAndEqual(ArrayList<Tile> tiles){
-        return tiles.stream().distinct().count() == 1 && !tiles.contains(Tile.Empty);
-    }
-    protected boolean notEmptyAndAllDifferent(ArrayList<Tile> tiles){
-        return !tiles.contains(Tile.Empty) && tiles.stream().distinct().count() == tiles.size();
-    }
-    protected ArrayList<Tile> allTilesInColumn(Shelf shelf, int column){
-        ArrayList<Tile> tiles = new ArrayList<>();
-        for(int row = 0; row < shelf.getRows();row++){
-            tiles.add(shelf.getTile((new Position(row,column))));
+    protected boolean notEmptyAndEqual(ArrayList<Tile> tiles) throws CommonGoalsException {
+        try {
+            return tiles.stream().distinct().count() == 1 && !tiles.contains(Tile.Empty);
+
+        } catch (NullPointerException e){
+            throw new CommonGoalsException("Error while checking notEmptyAndEqual : tiles is null pointer");
         }
-        return tiles;
     }
-    protected  ArrayList<Tile> allTilesInRow(Shelf shelf, int row){
-        ArrayList<Tile> tiles = new ArrayList<>();
-        for(int column = 0; column < shelf.getColumns();column++){
-            tiles.add(shelf.getTile((new Position(row,column))));
+    protected boolean notEmptyAndAllDifferent(ArrayList<Tile> tiles) throws CommonGoalsException {
+        try {
+            return !tiles.contains(Tile.Empty) && tiles.stream().distinct().count() == tiles.size();
+        } catch (NullPointerException e){
+            throw new CommonGoalsException("Error while checking notEmptyAndAllDifferent : tiles is null pointer");
         }
-        return tiles;
     }
-    protected boolean maxThreeTypes(ArrayList<Tile> tiles){
-        return tiles.size() > 0 &&
-                !tiles.contains(Tile.Empty) &&
-                tiles.stream().distinct().count() <= 3;
+    protected ArrayList<Tile> allTilesInColumn(Shelf shelf, int column) throws CommonGoalsException{
+        try {
+            ArrayList<Tile> tiles = new ArrayList<>();
+            for (int row = 0; row < shelf.getRows(); row++) {
+                tiles.add(shelf.getTile(row, column));
+            }
+            return tiles;
+        } catch (NullPointerException e){
+            throw new CommonGoalsException("Error while checking AllTilesInColumn : shelf is null pointer");
+        }
+    }
+    protected  ArrayList<Tile> allTilesInRow(Shelf shelf, int row) throws CommonGoalsException{
+        try {
+            ArrayList<Tile> tiles = new ArrayList<>();
+            for (int column = 0; column < shelf.getColumns(); column++) {
+                tiles.add(shelf.getTile(row, column));
+            }
+            return tiles;
+        } catch (NullPointerException e){
+            throw new CommonGoalsException("Error while checking AllTilesInRow : shelf is null pointer");
+        }
+    }
+    protected boolean maxThreeTypes(ArrayList<Tile> tiles) throws CommonGoalsException {
+        try {
+            return tiles.size() > 0 &&
+                    !tiles.contains(Tile.Empty) &&
+                    tiles.stream().distinct().count() <= 3;
+
+        } catch (NullPointerException e){
+            throw new CommonGoalsException("Error while checking maxThreeTypes : tiles is null pointer");
+        }
     }
 }
