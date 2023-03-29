5ca0efbbb6f717e9b97dc56d55ba824e11e3ad75
Giacomo Brunetta <giacomo.brunetta@mail.polimi.it>
2023-03-30 00:19:08 +0200

    Parametric commonGoal Predicates

--- src/main/java/it/polimi/ingsw/server/CommonGoalStrategy.java
+++ src/main/java/it/polimi/ingsw/server/CommonGoalStrategy.java
@@ -5,23 +5,24 @@ import it.polimi.ingsw.shared.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public enum CommonGoalStrategy {
-    EightEqualTiles(6, Predicates.NequalTiles(8)),
+    EightEqualTiles(6, Predicates.nEqualTiles(8)),
     EqualTilesInAllCorners(3, Predicates.equalTilesInAllCorners),
     EqualX(12, Predicates.equalX),
     FourGroupsOf4(11, Predicates.groups(4,4)),
-    FourLineWith3Types(8, Predicates.fourLineWith3Types),
+    FourLineWith3Types(8, Predicates.rows(4,Predicates.maxNtypes(3))),
     FullLadder(10, Predicates.fullLadder),
     Ladders(7, Predicates.ladder),
     SixGroupsOf2(1, Predicates.groups(6,2)),
-    ThreeColumnsWith3Types(4, Predicates.threeColumnsWith3Types),
-    TwoAllDifferentColumns(5, Predicates.twoAllDifferentColumns),
-    TwoAllDifferentLines(9, Predicates.twoAllDifferentLines),
-    TwoSquares(2, Predicates.twoSquares),
+    ThreeColumnsWith3Types(4, Predicates.columns(3,Predicates.maxNtypes(3))),
+    TwoAllDifferentColumns(5, Predicates.columns(2,Predicates.notEmptyAndAllDifferent)),
+    TwoAllDifferentLines(9, Predicates.rows(2,Predicates.notEmptyAndAllDifferent)),
+    TwoSquares(2, Predicates.nSquares(2)),
     ;
     private final int id;
     private final Predicate<Shelf> check;
@@ -46,107 +47,51 @@ public enum CommonGoalStrategy {
                 orElseThrow();
     }
 
+    //PREDICATES PASSED TO COMMON GOALS
     private static class Predicates {
-        static Predicate<Shelf> equalTilesInAllCorners = (shelf) -> {
-            return notEmptyAndEqual(shelf.getCorners());
-        };
-        static Predicate<Shelf> equalX = (shelf) -> {
-            int columns = shelf.getColumns();
-            int rows = shelf.getRows();
-            //Shelf too small to complete goal
-            if (columns < 3 || rows < 3) {
-                return false;
-            }
-            //Look for an "x" shape of tiles that are equal and not Empty
-            for (int row = 0; row < rows - 2; row++) {
-                for (int column = 0; column < columns - 2; column++) {
-                    if (notEmptyAndEqual(getXShape(shelf, row, column))) {
-                        return true;
-                    }
+        //PREDICATES ON GROUPS OF TILES (called by most of Predicate<Shelf>)
+        static Predicate<List<Tile>> notEmptyAndEqual = new Predicate<List<Tile>>() {
+            @Override
+            public boolean test(List<Tile> tiles) {
+                try {
+                    return tiles.stream().distinct().count() == 1 && !tiles.contains(Tile.Empty);
+
+                } catch (NullPointerException e) {
+                    throw new CommonGoalRuntimeException("Error while checking notEmptyAndEqual : tiles is null pointer");
                 }
             }
-            return false;
         };
-        static Predicate<Shelf> fourLineWith3Types = (shelf) -> {
-            Long result = IntStream.range(0, shelf.getRows())
-                    .filter(row -> maxNTypes(shelf.allTilesInRow(row), 3))
-                    .count();
-            return Math.toIntExact(result) >= 4;
-        };
-        static Predicate<Shelf> fullLadder = (shelf) -> {//get all heights in order
-            int[] heights = new int[shelf.getColumns()];
-            for (int column = 0; column < shelf.getColumns(); column++) {
-                heights[column] = columnHeigth(shelf, column);
-            }
-            //check if they form a ladder
-            return isLadder(heights);
-        };
-        static Predicate<Shelf> ladder = (shelf) -> {
-            //generate ladders
-            ArrayList<ArrayList<Tile>> ladders = null;
-            ladders = generateLadders(shelf);
-            //check if at least one is composed of all equal tiles
-            for (ArrayList<Tile> ladder : ladders) {
-                if (notEmptyAndEqual(ladder)) {
-                    return true;
+        static Predicate<List<Tile>> notEmptyAndAllDifferent = new Predicate<List<Tile>>() {
+            @Override
+            public boolean test(List<Tile> tiles) {
+                try {
+                    return !tiles.contains(Tile.Empty) && tiles.stream().distinct().count() == tiles.size();
+                } catch (NullPointerException e) {
+                    throw new CommonGoalRuntimeException("Error while checking notEmptyAndAllDifferent : tiles is null pointer");
                 }
             }
-            return false;
         };
-        static Predicate<Shelf> threeColumnsWith3Types = (shelf) -> {
-            Long result = IntStream.range(0, shelf.getColumns())
-                    .filter(column -> maxNTypes(shelf.allTilesInColumn(column), 3))
-                    .count();
-            return Math.toIntExact(result) >= 3;
-        };
-        static Predicate<Shelf> twoAllDifferentColumns = (shelf) -> {
-            Long result = IntStream.range(0, shelf.getColumns())
-                    .filter(column -> notEmptyAndAllDifferent(shelf.allTilesInColumn(column)))
-                    .count();
-            return Math.toIntExact(result) >= 2;
-        };
-        static Predicate<Shelf> twoAllDifferentLines = (shelf) -> {
-            Long result = IntStream.range(0, shelf.getRows())
-                    .filter(row -> notEmptyAndAllDifferent(shelf.allTilesInRow(row)))
-                    .count();
-            return Math.toIntExact(result) >= 2;
-        };
-        static Predicate<Shelf> twoSquares = (shelf) -> {
-            int columns = shelf.getColumns();
-            int rows = shelf.getRows();
-            int equalSquaresCounter = 0;
-            boolean[][] visited = new boolean[rows][columns];
-            //count all perfect squares (no overlap or adjacency)
-            for (int row = 0; row < rows - 1; row++) {
-                for (int column = 0; column < columns - 1; column++) {
-                    if (isPerfectSquare(shelf, visited, row, column)) {
-                        markSquareAsUsed(visited, row, column);
-                        equalSquaresCounter++;
-                    }
-                }
-            }
-            return equalSquaresCounter >= 2;
-        };
-        static Predicate<Shelf> groups(int number_of_groups, int size_of_groups){
-            return new Predicate<Shelf>() {
+        static Predicate<List<Tile>> maxNtypes(int n){
+            return new Predicate<List<Tile>>() {
                 @Override
-                public boolean test(Shelf shelf) {
-                    int rows = shelf.getRows();
-                    int columns = shelf.getColumns();
-                    boolean[][] alreadyChecked = new boolean[rows][columns];
-                    int groups_found = 0;
-                    for (int row = 0; row < rows; row++) {
-                        for (int column = 0; column < columns; column++) {
-                            if (validIslandSize(shelf, new Position(row, column), alreadyChecked) >= size_of_groups) {
-                                groups_found++;
-                            }
-                        }
+                public boolean test(List<Tile> tiles) {
+                    try {
+                        return tiles.size() > 0 &&
+                                !tiles.contains(Tile.Empty) &&
+                                tiles.stream().distinct().count() <= n;
+
+                    } catch (NullPointerException e) {
+                        throw new CommonGoalRuntimeException("Error while checking maxThreeTypes : tiles is null pointer");
                     }
-                    return groups_found >= number_of_groups;
                 }
             };
-        }
-        static Predicate<Shelf> NequalTiles(int n){
+        };
+
+        //ACTUAL PREDICATES
+        /**
+         * is true when the shelf contains n equal tiles
+         */
+        static Predicate<Shelf> nEqualTiles(int n){
             return new Predicate<Shelf>() {
                 @Override
                 public boolean test(Shelf shelf) {
@@ -174,55 +119,109 @@ public enum CommonGoalStrategy {
                 }
             };
         }
-        //static private functions used in predicates
 
         /**
-         * Checks if some tiles are all Equal and not Empty
-         *
-         * @param tiles an ArrayList of Tiles
-         * @return true if there are no Empty Tiles and they are all equal
+         * is true when n lines test true on the predicate
          */
-        static private boolean notEmptyAndEqual(ArrayList<Tile> tiles) {
-            try {
-                return tiles.stream().distinct().count() == 1 && !tiles.contains(Tile.Empty);
-
-            } catch (NullPointerException e) {
-                throw new CommonGoalRuntimeException("Error while checking notEmptyAndEqual : tiles is null pointer");
-            }
+        static Predicate<Shelf> rows(int number, Predicate<List<Tile>> toCheck){
+            return new Predicate<Shelf>(){
+                @Override
+                public boolean test(Shelf shelf){
+                    Long result = IntStream.range(0, shelf.getRows())
+                            .filter(row -> toCheck.test(shelf.allTilesInRow(row)))
+                            .count();
+                    return Math.toIntExact(result) >= number;
+                }
+            };
         }
 
         /**
-         * Checks if some tiles are all different and not Empty
-         *
-         * @param tiles an ArrayList of Tiles
-         * @return true if there are no Empty Tiles and they are all different
+         * is true when n columns test true on the predicate
          */
-        static private boolean notEmptyAndAllDifferent(ArrayList<Tile> tiles) {
-            try {
-                return !tiles.contains(Tile.Empty) && tiles.stream().distinct().count() == tiles.size();
-            } catch (NullPointerException e) {
-                throw new CommonGoalRuntimeException("Error while checking notEmptyAndAllDifferent : tiles is null pointer");
-            }
+        static Predicate<Shelf> columns(int number, Predicate<List<Tile>> toCheck){
+            return new Predicate<Shelf>(){
+                @Override
+                public boolean test(Shelf shelf){
+                    Long result = IntStream.range(0, shelf.getColumns())
+                            .filter(column -> toCheck.test(shelf.allTilesInColumn(column)))
+                            .count();
+                    return Math.toIntExact(result) >= number;
+                }
+            };
         }
 
         /**
-         * Checks if some Tiles have maximum n types of Tiles and do not contain Empty Tiles
-         *
-         * @param tiles an ArrayList of Tiles
-         * @param n     the maximum number of accepted types of Tiles in tiles
-         * @return True if there are no empty Tiles and there are maximum n types of Tiles in tiles
+         * is true when the shelf contains n perfect squares
          */
-        static private boolean maxNTypes(ArrayList<Tile> tiles, int n) {
+        static Predicate<Shelf> nSquares(int n){
+            return new Predicate<Shelf>(){
+                @Override
+                public boolean test(Shelf shelf){
+                    int columns = shelf.getColumns();
+                    int rows = shelf.getRows();
+                    int equalSquaresCounter = 0;
+                    boolean[][] visited = new boolean[rows][columns];
+                    //count all perfect squares (no overlap or adjacency)
+                    for (int row = 0; row < rows - 1; row++) {
+                        for (int column = 0; column < columns - 1; column++) {
+                            if (isPerfectSquare(shelf, visited, row, column)) {
+                                markSquareAsUsed(visited, row, column);
+                                equalSquaresCounter++;
+                            }
+                        }
+                    }
+                    return equalSquaresCounter >= 2;
+                }
+            };
+        }
+        static private ArrayList<Tile> get2x2Square(Shelf shelf, int row, int column) {
             try {
-                return tiles.size() > 0 &&
-                        !tiles.contains(Tile.Empty) &&
-                        tiles.stream().distinct().count() <= n;
-
-            } catch (NullPointerException e) {
-                throw new CommonGoalRuntimeException("Error while checking maxThreeTypes : tiles is null pointer");
+                ArrayList<Tile> tiles = new ArrayList<>();
+                tiles.add(shelf.getTile(row, column));
+                tiles.add(shelf.getTile(row + 1, column));
+                tiles.add(shelf.getTile(row, column + 1));
+                tiles.add(shelf.getTile(row + 1, column + 1));
+                return tiles;
+            } catch (BadPositionException e) {
+                throw new CommonGoalRuntimeException("Error in get2x2Square");
             }
         }
+        static private void markSquareAsUsed(boolean[][] visited, int row, int column) {
+            visited[row][column] = true;
+            visited[row + 1][column] = true;
+            visited[row][column + 1] = true;
+            visited[row + 1][column + 1] = true;
+        }
+        static private boolean isPerfectSquare(Shelf shelf, boolean[][] visited, int row, int column) {
+            return !(visited[row][column]) && !(visited[row + 1][column]) &&
+                    !(visited[row][column + 1]) && !(visited[row + 1][column + 1])
+                    &&
+                    notEmptyAndEqual.test(get2x2Square(shelf, row, column)) &&
+                    4 == validIslandSize(shelf, new Position(row, column), visited);
+        }
 
+        /**
+         * is true when the shelf contains n groups of the required size
+         */
+        static Predicate<Shelf> groups(int number_of_groups, int size_of_groups){
+            return new Predicate<Shelf>() {
+                @Override
+                public boolean test(Shelf shelf) {
+                    int rows = shelf.getRows();
+                    int columns = shelf.getColumns();
+                    boolean[][] alreadyChecked = new boolean[rows][columns];
+                    int groups_found = 0;
+                    for (int row = 0; row < rows; row++) {
+                        for (int column = 0; column < columns; column++) {
+                            if (validIslandSize(shelf, new Position(row, column), alreadyChecked) >= size_of_groups) {
+                                groups_found++;
+                            }
+                        }
+                    }
+                    return groups_found >= number_of_groups;
+                }
+            };
+        }
         static private int recursiveIslandVisit(Shelf shelf, Position position, boolean[][] visited, Tile type) {
             int row = position.getRow();
             int column = position.getColumn();
@@ -268,6 +267,33 @@ public enum CommonGoalStrategy {
             }
         }
 
+        /**
+         * is true when all corners of the shelf are not Empty and Equal
+         */
+        static Predicate<Shelf> equalTilesInAllCorners = (shelf) -> {
+            return Predicates.notEmptyAndEqual.test(shelf.getCorners());
+        };
+
+        /**
+         * is true when the shelf contains an "x" shape of non-empty and equals tiles
+         */
+        static Predicate<Shelf> equalX = (shelf) -> {
+            int columns = shelf.getColumns();
+            int rows = shelf.getRows();
+            //Shelf too small to complete goal
+            if (columns < 3 || rows < 3) {
+                return false;
+            }
+            //Look for an "x" shape of tiles that are equal and not Empty
+            for (int row = 0; row < rows - 2; row++) {
+                for (int column = 0; column < columns - 2; column++) {
+                    if (Predicates.notEmptyAndEqual.test(getXShape(shelf, row, column))) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        };
         static private ArrayList<Tile> getXShape(Shelf shelf, int row, int column) {
             try {
                 ArrayList<Tile> tiles = new ArrayList<>();
@@ -280,6 +306,22 @@ public enum CommonGoalStrategy {
             } catch (BadPositionException e) {
                 throw new CommonGoalRuntimeException("Error in getXshape");
             }
+        }
+
+        /**
+         * is true when the tiles form a ladder in the shelf
+         */
+        static Predicate<Shelf> fullLadder = (shelf) -> {//get all heights in order
+            int[] heights = new int[shelf.getColumns()];
+            for (int column = 0; column < shelf.getColumns(); column++) {
+                heights[column] = columnHeigth(shelf, column);
+            }
+            //check if they form a ladder
+            return isLadder(heights);
+        };
+        static private int columnHeigth(Shelf shelf, int column){
+            return (int) shelf.allTilesInColumn(column).stream()
+                    .filter(x -> !x.equals(Tile.Empty)).count();
         }
         static private boolean isLadder(int[] heights) {
             if (heights.length == 1) {
@@ -303,11 +345,21 @@ public enum CommonGoalStrategy {
             return ascending || descending;
         }
 
-        static private int columnHeigth(Shelf shelf, int column){
-            return (int) shelf.allTilesInColumn(column).stream()
-                    .filter(x -> !x.equals(Tile.Empty)).count();
-        }
-
+        /**
+         * is true when the shelf contains a ladder of non-empty and equal tiles
+         */
+        static Predicate<Shelf> ladder = (shelf) -> {
+            //generate ladders
+            ArrayList<ArrayList<Tile>> ladders = null;
+            ladders = generateLadders(shelf);
+            //check if at least one is composed of all equal tiles
+            for (ArrayList<Tile> ladder : ladders) {
+                if (Predicates.notEmptyAndEqual.test(ladder)) {
+                    return true;
+                }
+            }
+            return false;
+        };
         static private ArrayList<ArrayList<Tile>> generateLadders(Shelf shelf) {
             int rows = shelf.getRows();
             int columns = shelf.getColumns();
@@ -341,34 +393,5 @@ public enum CommonGoalStrategy {
             }
             return ladders;
         }
-
-        static private ArrayList<Tile> get2x2Square(Shelf shelf, int row, int column) {
-            try {
-                ArrayList<Tile> tiles = new ArrayList<>();
-                tiles.add(shelf.getTile(row, column));
-                tiles.add(shelf.getTile(row + 1, column));
-                tiles.add(shelf.getTile(row, column + 1));
-                tiles.add(shelf.getTile(row + 1, column + 1));
-                return tiles;
-            } catch (BadPositionException e) {
-                throw new CommonGoalRuntimeException("Error in get2x2Square");
-            }
-        }
-        static private void markSquareAsUsed(boolean[][] visited, int row, int column) {
-            visited[row][column] = true;
-            visited[row + 1][column] = true;
-            visited[row][column + 1] = true;
-            visited[row + 1][column + 1] = true;
-        }
-
-        static private boolean isPerfectSquare(Shelf shelf, boolean[][] visited, int row, int column) {
-            return !(visited[row][column]) && !(visited[row + 1][column]) &&
-                    !(visited[row][column + 1]) && !(visited[row + 1][column + 1])
-                    &&
-                    notEmptyAndEqual(get2x2Square(shelf, row, column)) &&
-                    4 == validIslandSize(shelf, new Position(row, column), visited);
-        }
     }
 }
-
-
