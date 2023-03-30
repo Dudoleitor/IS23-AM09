498cb1f00c46bfbb025b3895a0262209b8aabc5f
Giacomo Brunetta <102242995+giacomo-brunetta@users.noreply.github.com>
2023-03-30 08:59:18 +0100

    Abstract factory proposal (interface declaration only)

--- src/main/java/it/polimi/ingsw/server/CommonGoalsFactory.java
+++ src/main/java/it/polimi/ingsw/server/CommonGoalsFactory.java
@@ -7,7 +7,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
