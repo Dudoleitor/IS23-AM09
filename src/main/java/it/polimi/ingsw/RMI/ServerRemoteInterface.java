a90842cd1d0d9b95bc953fcc8edb63d5347de341
Mik-Ado <125997032+Mik-Ado@users.noreply.github.com>
2023-04-04 11:37:06 +0200

    Renamed RemoteInterface

--- src/main/java/it/polimi/ingsw/RMI/ServerRemoteInterface.java
+++ src/main/java/it/polimi/ingsw/RMI/ServerRemoteInterface.java
@@ -1,23 +1,21 @@
 package it.polimi.ingsw.RMI;
 import it.polimi.ingsw.server.Move;
-import it.polimi.ingsw.shared.Color;
 import it.polimi.ingsw.shared.JsonBadParsingException;
-import it.polimi.ingsw.shared.Shelf;
 import org.json.simple.JSONObject;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
-public interface RemoteCall extends Remote {
+public interface ServerRemoteInterface extends Remote {
     boolean login(String nick) throws RemoteException;
     void sendShelf(JSONObject s) throws RemoteException, JsonBadParsingException;
     void postToLiveChat(String playerName, String message) throws Exception;
     List<ChatMessage> updateLiveChat(String player, int alreadyReceived) throws RemoteException;
 
-    void joinLobby(String player, RemoteCall stub) throws RemoteException;
-    void createLobby(String player, RemoteCall stub,  int numPlayers) throws RemoteException;
-    void quitGame(String player, RemoteCall stub) throws RemoteException;
+    void joinLobby(String player, ServerRemoteInterface stub) throws RemoteException;
+    void createLobby(String player, ServerRemoteInterface stub, int numPlayers) throws RemoteException;
+    void quitGame(String player, ServerRemoteInterface stub) throws RemoteException;
     boolean matchHasStarted(String player) throws RemoteException;
     boolean isMyTurn(String player) throws RemoteException;
     List<Move> getValidMoves(String player) throws RemoteException;
