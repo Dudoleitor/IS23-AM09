4a2f13aa3bfd07a6268683e8fd820914ec72011b
Giacomo Brunetta <102242995+giacomo-brunetta@users.noreply.github.com>
2023-04-17 14:14:09 +0100

    JoinLobby (abstract class) now handles connections and run method

--- src/main/java/it/polimi/ingsw/client/JoinLobby/JoinLobbyCLI.java
+++ src/main/java/it/polimi/ingsw/client/JoinLobby/JoinLobbyCLI.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.client.JoinLobby;
 
 import it.polimi.ingsw.client.InputSanitizer;
-import it.polimi.ingsw.client.Lobby.LobbyCLI;
 import it.polimi.ingsw.client.Lobby.Lobby;
+import it.polimi.ingsw.client.Lobby.LobbyCLI;
+import it.polimi.ingsw.client.Lobby.LobbyUI;
 import it.polimi.ingsw.client.cli_IO;
-import it.polimi.ingsw.server.clientonserver.Client;
 
 import java.util.List;
 import java.util.Map;
@@ -13,38 +13,28 @@ import java.util.stream.Collectors;
 public class JoinLobbyCLI extends JoinLobbyUI{
     private cli_IO io = new cli_IO();
     private InputSanitizer inputSanitizer = new InputSanitizer();
-    private Client client;
 
     public JoinLobbyCLI(Server server) {
         super(server);
     }
-
     @Override
-    public void run(){
-        setUserName();
-        client = server.generateClient(playerName);
-        tryLogin(client,30);
-        showLobbyList();
-        showJoinedLobbies();
-        joinLobby(client);
-    }
-
-    @Override
-    public void setUserName(){
-        while(!inputSanitizer.isValidName(playerName)){
+    public String askUserName(){
+        String name = "";
+        while(!inputSanitizer.isValidName(name)){
             io.printMessage("Enter your username");
-            playerName = io.scan();
-            if(!inputSanitizer.isValidName(playerName)){
+            name = io.scan();
+            if(!inputSanitizer.isValidName(name)){
                 io.printErrorMessage("Please enter a valid name");
             }
         }
+        return name;
     }
     @Override
-    public boolean tryLogin(Client client,int tries){
+    public boolean tryLogin(int tries){
         boolean logged = false;
         //Try to log in for 30s (1 try each second)
         for(int attempt = 0; attempt < tries && !logged; tries++){
-            logged = server.login(client); //get previous sessions if present
+            logged = login(); //get previous sessions if present
             if(logged) {
                 showJoinedLobbies();
             }
@@ -59,7 +49,7 @@ public class JoinLobbyCLI extends JoinLobbyUI{
         }
         return logged;
     }
-
+    @Override
     public void showJoinedLobbies(){
         List<Integer> previousSessions = server.getJoinedLobbies(playerName);
         if (previousSessions != null && !previousSessions.isEmpty()) { //if some previous session is available
@@ -74,35 +64,7 @@ public class JoinLobbyCLI extends JoinLobbyUI{
             io.printMessage(message);
         }
     }
-
-    public void joinLobby(Client client){
-        boolean lobbySelected = false;
-        int lobbyID;
-        Lobby lobby = null;
-        while(!lobbySelected){
-            io.printMessage("Choose a Lobby (ENTER for random):");
-            String id = io.scan();
-            if(id.isEmpty()){
-                lobby = server.joinRandomLobby(client); //join first available lobby, otherwise creates one
-                lobbySelected = true;
-            } else if(inputSanitizer.isInteger(id)){
-                lobbyID = Integer.parseInt(id);
-                lobby = server.joinSelectedLobby(client, lobbyID);
-                if(lobby == null)
-                    io.printErrorMessage("Input id not found");
-                else lobbySelected = true;
-            }
-            else{
-                io.printErrorMessage("Input a valid id (must be a number)");
-            }
-        }
-        io.printMessage("You joined #"+ lobby.getID()+" lobby!");
-
-        //Create the lobbyUI object and start the match
-        LobbyCLI lobbyCLI = new LobbyCLI(playerName, lobby);
-        lobbyCLI.run();
-    }
-
+    @Override
     public void showLobbyList(){
         Map availableLobbies = server.showAvailableLobbbies();
         if (!availableLobbies.isEmpty()) {
@@ -117,6 +79,52 @@ public class JoinLobbyCLI extends JoinLobbyUI{
             io.printMessage(lobbyMessage);
         } else {
             io.printMessage("No other active lobby is available, you might want to create one");
+        }
+    }
+    @Override
+    public LobbyUI getLobbyUI(){
+        LobbySelectionCommand command = LobbySelectionCommand.Invalid;
+        Lobby lobby = null;
+        while(command == LobbySelectionCommand.Invalid){
+            command = askLobby();
+            lobby = joinLobby(command);
+            if(command == LobbySelectionCommand.Invalid){
+                io.printErrorMessage("Input a valid id (must be a number)");
+            }
+        }
+        io.printMessage("You joined #"+ lobby.getID()+" lobby!");
+        //Create the lobbyUI object and start the match
+        return new LobbyCLI(playerName,lobby);
+    }
+    @Override
+    public LobbySelectionCommand askLobby(){
+        int lobbyID;
+        LobbySelectionCommand command;
+        io.printMessage("Choose a Lobby (ENTER for random):");
+        String id = io.scan();
+        if(id.isEmpty()){
+            return LobbySelectionCommand.Random;
+        } else if(inputSanitizer.isInteger(id)){
+            lobbyID = Integer.parseInt(id);
+            command = LobbySelectionCommand.Number;
+            command.setId(lobbyID);
+        }
+        else{
+            command = LobbySelectionCommand.Invalid;
+            io.printErrorMessage("Input a valid id (must be a number)");
+        }
+        return command;
+    }
+    @Override
+    public boolean playAgain(){
+        String answer;
+        io.printMessage("Do you want to play again?");
+        answer = io.scan();
+        if(answer.equals("yes") || answer.equals("y")){
+            return true;
+        }
+        else {
+            return false;
         }
     }
 }
--- src/main/java/it/polimi/ingsw/client/JoinLobby/JoinLobbyUI.java
+++ src/main/java/it/polimi/ingsw/client/JoinLobby/JoinLobbyUI.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.client.JoinLobby;
 
+import it.polimi.ingsw.client.Lobby.Lobby;
+import it.polimi.ingsw.client.Lobby.LobbyUI;
 import it.polimi.ingsw.server.clientonserver.Client;
 
 public abstract class JoinLobbyUI extends Thread{
@@ -7,6 +9,7 @@ public abstract class JoinLobbyUI extends Thread{
     protected String playerName;
     protected Server server;
     protected Client client;
+    protected LobbyUI match;
 
     //Constructor
     JoinLobbyUI(Server server){
@@ -14,9 +17,57 @@ public abstract class JoinLobbyUI extends Thread{
     }
 
     //Methods
-    public abstract void setUserName();
-    public abstract boolean tryLogin(Client client, int tries);
+    public abstract String askUserName();
+    public abstract boolean tryLogin(int tries);
+    public boolean login(){
+        return server.login(client);
+    }
+    public abstract LobbyUI getLobbyUI();
+    public abstract LobbySelectionCommand askLobby();
+    public Lobby joinLobby(LobbySelectionCommand command){
+        switch (command){
+            case Random:
+                return server.joinRandomLobby(client);
+            case Number:
+                return server.joinSelectedLobby(client,command.getID());
+            default:
+                return null;
+        }
+    }
     public abstract void showJoinedLobbies();
-    public abstract void joinLobby(Client client);
     public abstract void showLobbyList();
+    public abstract boolean playAgain();
+    @Override
+    public void run(){
+        boolean play = true;
+        while(play){
+            playerName = askUserName();
+            client = server.generateClient(playerName);
+            tryLogin(30);
+            showLobbyList();
+            showJoinedLobbies();
+            LobbyUI match = getLobbyUI();
+            match.start();
+            try {
+                match.join();
+            } catch (InterruptedException e) {
+                play = false;
+            }
+            play = playAgain();
+        }
+    }
+
+    protected enum LobbySelectionCommand{
+        Invalid(),
+        Number(),
+        Random();
+        private int id = -1;
+        public void setId(int id){
+            this.id = id;
+        }
+        public int getID(){
+            return id;
+        }
+    }
+
 }
