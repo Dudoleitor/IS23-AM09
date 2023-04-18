0da5c85c3e159e5209ed641974c73ced71774462
CantonettiGianluca <gianluca.cantonetti@mail.polimi.it>
2023-04-18 09:34:11 +0200

    Added a basic (JavaFX) form that asks the user to insert the userName and the number of the lobby

--- src/main/java/it/polimi/ingsw/client/gui/HelloApplication.java
+++ src/main/java/it/polimi/ingsw/client/gui/HelloApplication.java
@@ -1,8 +1,20 @@
 package it.polimi.ingsw.client.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +22,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("gui/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        Parent root = fxmlLoader.load(getClass().getClassLoader().getResource("gui/hello-view.fxml"));
+        Scene scene = new Scene(root, 320, 240, Color.GREEN);
+        stage.setTitle("My Shelfie");
         stage.setScene(scene);
         stage.show();
     }
