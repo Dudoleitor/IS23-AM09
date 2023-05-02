c6b2b3d6c6e8c8ad45d154f72ae451a90a82de1a
CantonettiGianluca <gianluca.cantonetti@mail.polimi.it>
2023-05-02 14:53:20 +0200

    Added simple feature in PlayerHomeScreen: now if you click mouse in PlayerHomeScreen it generates a random PersonalGoal and displays it.

--- src/main/java/it/polimi/ingsw/client/View/gui/homeScreenController.java
+++ src/main/java/it/polimi/ingsw/client/View/gui/homeScreenController.java
@@ -2,13 +2,36 @@ package it.polimi.ingsw.client.View.gui;
 import it.polimi.ingsw.client.controller.ClientControllerGUI;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 import java.io.IOException;
+import java.util.Random;
 
 public class homeScreenController {
 
     private ClientGUI client = new ClientGUI();
+
+    @FXML
+    ImageView imgPersGoal;
+
+    //qua mi servirà una funzione che mi dà il personal goal del player
+    private void randPG() {
+        Random rand = new Random();
+        int int_random = rand.nextInt(12);
+
+        if(int_random == 0) {
+            imgPersGoal.setImage(new Image("gui/gameGraphics/personal_goal_cards/Personal_Goals.png"));
+        } else {
+            imgPersGoal.setImage(new Image("gui/gameGraphics/personal_goal_cards/Personal_Goals" + int_random + ".png"));
+        }
+    }
+
+    @FXML
+    protected void randPersonalGoal() {
+        randPG();
+    }
 
     @FXML
     Button sh00;
