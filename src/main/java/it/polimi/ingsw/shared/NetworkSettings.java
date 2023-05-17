7cceb4fea37dd899682542160639b83045ff281a
Giacomo Brunetta <102242995+giacomo-brunetta@users.noreply.github.com>
2023-05-17 15:45:43 +0100

    Removed hardcoded default ip and ports

--- src/main/java/it/polimi/ingsw/shared/NetworkSettings.java
+++ src/main/java/it/polimi/ingsw/shared/NetworkSettings.java
@@ -3,17 +3,9 @@ package it.polimi.ingsw.shared;
 import org.apache.commons.cli.ParseException;
 
 public class NetworkSettings {
-    public static int RMIport = 1234;
-    public static int TCPport = 1235;
-    public static IpAddressV4 serverIp = createIp();
-    private static IpAddressV4 createIp(){
-        try{
-            return new IpAddressV4("127.0.0.1");
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    public static int RMIport;
+    public static int TCPport;
+    public static IpAddressV4 serverIp;
     public static final int WaitingTime = 2000; //the time to wait for a server answer
-
     public static final long serverPingIntervalSeconds = 3;
 }
