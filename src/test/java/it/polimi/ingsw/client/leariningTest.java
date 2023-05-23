d4da72740c41267a04605ed2cee82aa1fd6aae8e
Giacomo Brunetta <102242995+giacomo-brunetta@users.noreply.github.com>
2023-05-23 09:57:43 +0200

    fixed bug in main cli parsing that set ports to 0

---
+++ src/test/java/it/polimi/ingsw/client/leariningTest.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.client;
+
+import org.junit.jupiter.api.Test;
+
+public class leariningTest {
+    @Test
+    void tryParseInt(){
+        int culo = Integer.parseInt(null);
+    }
+}
